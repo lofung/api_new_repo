877ca5691dfe1a81a04c827ec95945b80577289a
lychenus <lovekillua@gmail.com>
2023-07-21 08:12:56 -0400

    first commit again

---
+++ MvcMovie/controll.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie
+{
+    public class controll : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
